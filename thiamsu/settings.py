62e56bc164ae1369da15e924bf05ee140fbf7247
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-05 00:40:46 +0800

    Add Django app "meta"

--- thiamsu/settings.py
+++ thiamsu/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # third-party
     'user.apps.UserConfig',
     'social_django',
+    # local
+    'meta.apps.MetaConfig'
 ]
 
 MIDDLEWARE = [
