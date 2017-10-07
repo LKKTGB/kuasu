ebcc7904d4d57404b36d32adec664b9c04a0ea95
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-07 22:32:58 +0800

    Add social auth settings about version and pipeline

--- thiamsu/settings.py
+++ thiamsu/settings.py
@@ -66,6 +66,17 @@ LOGIN_REDIRECT_URL = 'profile'
 
 SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY')
 SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET')
+SOCIAL_AUTH_FACEBOOK_API_VERSION = '2.10'
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
 
 ROOT_URLCONF = 'thiamsu.urls'
 
