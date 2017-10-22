f77cbc83dfc3499d07c7d5adb734ac079a1f0d51
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-22 15:37:19 +0800

    Add field "original_lyrics" to Song for original lyrics

--- meta/models/song.py
+++ meta/models/song.py
@@ -8,3 +8,4 @@ class Song(models.Model):
     hanlo_title = models.CharField(max_length=100, help_text='漢羅歌名')
     singer = models.CharField(max_length=100, help_text='演唱人')
     youtube_url = models.CharField(max_length=100, help_text='Youtube 網址')
+    original_lyrics = models.TextField(default='', help_text='原文歌詞')
