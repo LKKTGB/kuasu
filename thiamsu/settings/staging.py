e45a88189af7db0b7099e4fc163c8432de5e3ec6
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-28 20:26:15 +0800

    Setup DB on Heroku

--- thiamsu/settings/staging.py
+++ thiamsu/settings/staging.py
@@ -1,10 +1,14 @@
 from thiamsu.settings.base import *
+import dj_database_url
 
 '''
 Settings for staging environment deployed on Heroku
 '''
 
 ALLOWED_HOSTS = ['*']
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
