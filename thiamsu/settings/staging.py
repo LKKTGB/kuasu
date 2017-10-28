095ce0d59ff8f6cef274ad65deff168463ee29b6
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-28 19:00:19 +0800

    Setup STATIC_ROOT and STATICFILES_DIRS on staging

--- thiamsu/settings/staging.py
+++ thiamsu/settings/staging.py
@@ -1,1 +1,10 @@
 from thiamsu.settings.base import *
+
+'''
+Settings for staging environment deployed on Heroku
+'''
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
