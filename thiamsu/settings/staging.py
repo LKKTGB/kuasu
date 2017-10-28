9241056c6ff32fdb72980a239e151ddfa4543942
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-28 19:27:55 +0800

    Add '*' to ALLOWED_HOSTS on staging

--- thiamsu/settings/staging.py
+++ thiamsu/settings/staging.py
@@ -4,6 +4,8 @@ from thiamsu.settings.base import *
 Settings for staging environment deployed on Heroku
 '''
 
+ALLOWED_HOSTS = ['*']
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
