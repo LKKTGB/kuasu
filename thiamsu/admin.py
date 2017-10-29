3dbf334bd28c1caccd6ccc8f73b3c1ecf5339c02
badboy99tw <taiwan.badboy99@gmail.com>
2017-10-29 22:37:01 +0800

    Setup admin for Translation

--- thiamsu/admin.py
+++ thiamsu/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 from thiamsu.models.song import Song
+from thiamsu.models.translation import Translation
 
 
 class SongAdmin(admin.ModelAdmin):
     list_display = ('original_title', 'singer')
 
 
+class TranslationAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Song, SongAdmin)
+admin.site.register(Translation, TranslationAdmin)
