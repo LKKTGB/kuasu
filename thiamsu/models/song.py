e48fa2c41e87e96754fa3549a497125313a9eb8e
badboy99tw <taiwan.badboy99@gmail.com>
2017-11-12 02:28:16 +0800

    Add fields composer and lyricist

--- thiamsu/models/song.py
+++ thiamsu/models/song.py
@@ -9,6 +9,8 @@ class Song(models.Model):
     tailo_title = models.CharField(max_length=100, help_text='全羅歌名')
     hanlo_title = models.CharField(max_length=100, help_text='漢羅歌名')
     performer = models.CharField(max_length=100, help_text='演唱人')
+    composer = models.CharField(max_length=100, help_text='作曲人')
+    lyricist = models.CharField(max_length=100, help_text='作詞人')
     youtube_url = models.CharField(max_length=100, help_text='Youtube 網址')
     original_lyrics = models.TextField(default='', help_text='原文歌詞')
 
