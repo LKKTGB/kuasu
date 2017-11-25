eef3a97c611f97f5c8528d5df7d6c2d3126736db
badboy99tw <taiwan.badboy99@gmail.com>
2017-11-25 12:34:45 +0800

    Setup Django translation string for zh_Hant

--- thiamsu/settings/base.py
+++ thiamsu/settings/base.py
@@ -138,7 +138,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
+LANGUAGE_CODE = 'zh-Hant'
 
 TIME_ZONE = 'UTC'
 
