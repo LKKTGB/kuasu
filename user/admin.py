17aaa17225d633e0f217d69276642624dcb946d6
badboy99tw <taiwan.badboy99@gmail.com>
2017-11-26 02:29:46 +0800

    Show only necessary fields of user

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 from user.models.profile import Profile
 
@@ -18,6 +19,15 @@ class ProfileInline(admin.StackedInline):
 
 
 class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_superuser')}),
+    )
+    readonly_fields = ('username',)
+
     inlines = (ProfileInline, )
 
     def get_inline_instances(self, request, obj=None):
