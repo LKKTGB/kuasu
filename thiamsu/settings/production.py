5173e08b1fd0e16a2318e4c8dea3ac2412d2f4a8
badboy99tw <taiwan.badboy99@gmail.com>
2018-04-12 07:31:27 +0800

    Setup logging on production

--- thiamsu/settings/production.py
+++ thiamsu/settings/production.py
@@ -25,3 +25,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
