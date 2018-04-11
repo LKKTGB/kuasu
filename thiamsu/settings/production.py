679a62e5d6ea01ca0cbcf78ae7a3758e7d94bfdc
badboy99tw <taiwan.badboy99@gmail.com>
2018-04-12 07:16:06 +0800

    Run production on Heroku, copy settings from staging to production

--- thiamsu/settings/production.py
+++ thiamsu/settings/production.py
@@ -1,9 +1,23 @@
 from thiamsu.settings.base import *
+import dj_database_url
 
+'''
+Settings for production environment deployed on Heroku
+'''
 GA_TRACKING_ID = 'UA-114678735-2'
+
+ALLOWED_HOSTS = ['*']
 
 # WhiteNoise
 MIDDLEWARE.extend([
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ])
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
