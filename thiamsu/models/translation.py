2603b3fccae0558a0227ef2d6b787e5bd35ca654
badboy99tw <taiwan.badboy99@gmail.com>
2018-04-18 20:54:48 +0800

    Add language hanlo 漢羅

--- thiamsu/models/translation.py
+++ thiamsu/models/translation.py
@@ -11,6 +11,7 @@ class Translation(models.Model):
     LANG_CHOICES = (
         ('tailo', _('translation_lang_tailo')),
         ('hanzi', _('translation_lang_hanzi')),
+        ('hanlo', _('translation_lang_hanlo')),
     )
 
     song = models.ForeignKey(Song, verbose_name=_('song'))
