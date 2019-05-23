2353c253a1340c5fa732c74d15548e0ba6f0ac79
badboy99tw <taiwan.badboy99@gmail.com>
2019-05-24 01:56:25 +0800

    Apply whitenoise on testing to serve static files

--- thiamsu/settings/testing.py
+++ thiamsu/settings/testing.py
@@ -3,10 +3,14 @@ import dj_database_url
 from thiamsu.settings.base import *
 
 """
-Settings for testing environment deployed on Heroku
+Settings for testing environment deployed on Azure App Service
 """
 
 ALLOWED_HOSTS = ["thiamsu-testing.herokuapp.com", "kuasu-testing.azurewebsites.net"]
+
+# WhiteNoise
+MIDDLEWARE.extend(["whitenoise.middleware.WhiteNoiseMiddleware"])
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
